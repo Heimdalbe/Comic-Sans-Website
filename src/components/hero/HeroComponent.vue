027e13d3961b8c354fe16d2be8b7855d51467d9a
De Bock Benny <bdebock@protonmail.com>
2024-07-14 21:44:13 +0200

    Start on the Hero component

---
+++ src/components/hero/HeroComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <section id="hero">
+    I need a hero
+  </section>
+</template>
+
+<style scoped>
+#hero {
+  min-height: 100dvh - var(--header-height);
+}
+</style>
---
+++ src/components/navigation/HeaderComponent.vue
@@ -0,0 +1,27 @@
+<!-- import { RouterLink, RouterView } from 'vue-router' -->
+<template>
+  <header>
+    <nav>
+    This is a header component
+    </nav>
+  </header>
+
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header> -->
+</template>
+
+<style scoped>
+header {
+  height: var(--header-height);
+}
+</style>
