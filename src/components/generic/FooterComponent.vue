2c88a0a2146da0de818c91e3f6dc77d0a8be8cc6
De Bock Benny <bdebock@protonmail.com>
2024-08-05 22:59:31 +0200

    Add social media section to footer

--- src/components/generic/FooterComponent.vue
+++ src/components/generic/FooterComponent.vue
@@ -1,5 +1,20 @@
 <template>
   <footer>
-    This is a footer
+    <section id="social-media">
+      <h2 class="text-white">Social Media</h2>
+      <a href="https://www.instagram.com/cafecomicsans/" target="_blank"
+        class="inline-block size-10">
+        <InstagramIcon />
+      </a>
+      <a href="https://www.facebook.com/cafecomicsans/" target="_blank"
+        class="inline-block size-10">
+        <FacebookIcon />
+      </a>
+    </section>
   </footer>
 </template>
+
+<script setup lang="ts">
+import InstagramIcon from '../icons/InstagramIcon.vue';
+import FacebookIcon from '../icons/FacebookIcon.vue';
+</script>
